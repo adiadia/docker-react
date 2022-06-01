a048841d96ea5ce34ef7a2756c8894882b50d1de
Aditya <adiaditiwari@gmail.com>
2022-06-01 20:02:48 +0530

    inital commit

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  const gt = 4;
+  expect(gt).toEqual(4);
+});
+
+test("renders learn react link", () => {
+  const gt = 4;
+  expect(gt).toEqual(4);
+});
